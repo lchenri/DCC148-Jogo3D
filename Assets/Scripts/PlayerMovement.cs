23aa40fb8f1397f56a92fd8321d39a7e0fa4c838
lchenri <lucasbonicardoso@outlook.com>
2023-12-18 17:40:58 -0300

    Adicao do player teste com animacoes de pulo, corrida, parada e idle

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,16 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 3f;
     public float horizontalSpeed = 4f;
 
+    void MovimentaEsquerda()
+    {
+        transform.Translate(Vector3.left * horizontalSpeed * Time.deltaTime);
+    }
+
+    void MovimentaDireita()
+    {
+        transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
+    }
+
     void Update()
     {
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.World);
@@ -14,13 +24,17 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             if (transform.position.x > LevelBounds.leftBound)
-                transform.Translate(Vector3.left * horizontalSpeed * Time.deltaTime);
+            {
+                MovimentaEsquerda();
+            }
         }
 
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             if (transform.position.x < LevelBounds.rightBound)
-                transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
+            {
+                MovimentaDireita();
+            }
         }
     }
 }
